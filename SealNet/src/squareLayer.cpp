d2f485f77db33b6605b714d51e814c96cf51dc48
Carmen Barletta <barlettacarmen@gmail.com>
2018-08-16 19:36:43 +0200

    Fixing square by decrypting before other computations

--- SealNet/src/squareLayer.cpp
+++ SealNet/src/squareLayer.cpp
@@ -19,16 +19,28 @@ SquareLayer::~SquareLayer(){}
 ciphertext3D SquareLayer::forward (ciphertext3D input){
 	int x_size=input[0].size(), y_size=input[0][0].size(), z_size=input.size();
 	int z,x,y;
+	floatCube image(50, vector<vector<float> > (5,vector<float>(5)));
+	Plaintext tmp;
 
-	cout << "Size of a fresh encryption: " << input[0][0][0].size() << endl;
+
+
+	cout << "Size of input encryption: " << input[0][0][0].size() << endl;
     cout << "Noise budget in fresh encryption: "
         << decryptor->invariant_noise_budget(input[0][0][0]) << " bits" << endl;
+
+    	//provo a decriptare e recriptare prima della square
+	image=decryptImage(input);
 
 	for(z=0;z<z_size;z++)
 		for(x=0;x<x_size;x++)
 			for(y=0;y<y_size;y++){
 				assert(decryptor->invariant_noise_budget(input[z][x][y])>0);
+
+				tmp=fraencoder->encode(image[z][x][y]);
+				encryptor->encrypt(tmp,input[z][x][y]);
+				////
 				evaluator->square(input[z][x][y]);
+				//evaluator->multiply(input[z][x][y],input[z][x][y]);
 
 				cout << "Size after squaring: " << input[z][x][y].size() << endl;
 				cout << "Noise budget after squaring: "
