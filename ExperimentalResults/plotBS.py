f01426aa26e4f6b80cf3a795242fa53942b0016a
Carmen Barletta <barlettacarmen@gmail.com>
2018-11-28 14:08:25 +0100

    Adding some results

--- ExperimentalResults/plotBS.py
+++ ExperimentalResults/plotBS.py
@@ -7,13 +7,15 @@ import seaborn as snb
 
 name_ar=["BATCH_SIZE","PLAIN_MOD","TIME"]
 df=pd.read_csv(sys.argv[1],header=None,sep=',',names=name_ar)
+m={2:'o',4:'*',8:'D',16:'x',32:'^'}
 
 #print(df)
 plt.show()
+plt.grid(linestyle='-.')
 for k,subdf in df.groupby(["BATCH_SIZE"]):
 	#plt.scatter( [k for i in range(len(subdf)) ], [log2(el) for el in subdf['PLAIN_MOD']], label=str(k) )
-	plt.scatter( [k for i in range(1)] , [log2(np.max(subdf['PLAIN_MOD']))],label=str(k) )
-	#plt.scatter( [k for i in range(2)] , [log2(np.max(subdf['PLAIN_MOD'])), log2(np.mean(subdf['PLAIN_MOD']))],label=str(k) )
+	#plt.scatter( [k for i in range(1)] , [log2(np.max(subdf['PLAIN_MOD']))],label=str(k) )
+	plt.scatter( [k for i in range(2)] , [log2(np.max(subdf['PLAIN_MOD'])), log2(np.mean(subdf['PLAIN_MOD']))],label=str(k), marker=m[k],s=70)
 	#plt.scatter( [k for i in range(3)] , [log2(np.min(subdf['PLAIN_MOD'])),log2(np.mean(subdf['PLAIN_MOD'])),log2(np.max(subdf['PLAIN_MOD']))] , label=str(k) )
 	print(k,'. n elems:', len(subdf))
 
@@ -22,8 +24,9 @@ for k,subdf in df.groupby(["BATCH_SIZE"]):
 # 	plt.scatter( np.mean(subdf['TIME']), log2(np.max(subdf['PLAIN_MOD'])), label=str(k) )
 # 	print(k,'. n elems:', len(subdf))
 
+
 plt.xlabel("BATCH_SIZE")
-plt.ylabel("log2(max(PLAIN_MOD_FOUND))")
+plt.ylabel("log2(mean(t_found)) vs log2(max(t_found)) ")
 plt.xscale('log',basex=2)
 plt.legend()
 plt.show()
--- ExperimentalResults/timings.py
+++ ExperimentalResults/timings.py
@@ -5,7 +5,24 @@ import sys
 from math import log2
 import seaborn as snb
 
-name_ar=["I","T1","TR1","T2","TR2","T3","TR3","T4","TR4","T5","TR5","T6","TR6","T7","TR7","T8","TR8","T9","TR9","T10","TR10","T_ENC","T_DEC","PREDICTION"]
-df=pd.read_csv(sys.argv[1],index_col=False,sep=',',names=name_ar)
-print(df)
+#name_ar=["I","T1","T2","T3","T4","T5","TR","T6","T7","T8","T9","PREDICTION"]
+name_ar=["I","T1","T2","T3","T4","TR","T5","T6","PREDICTION"]
+df=pd.read_csv(sys.argv[1],index_col=False,sep=',',names=name_ar,)
+
 print(df.describe())
+
+#df["TOT"]=df[["T1","T2","T3","T4","T5","TR","T6","T7","T8","T9"]].sum(axis=1)
+df["TOT"]=df[["T1","T2","T3","T4","TR","T5","T6"]].sum(axis=1)
+print(np.mean(df["TOT"])*10**-6)
+
+
+snb.distplot(df["TOT"])
+plt.show()
+
+# time=0
+# var=0
+# for c in range(1,11):
+# 	print(df.ix[:,c].name)
+# 	time+=np.mean(df.ix[:,c])
+
+# print(time*10**-6)
